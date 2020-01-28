69db22b9bd0d6ec0ac988eee6dc2a287df1814c0
Yahya  Gok <ygok17@hotmail.com>
2020-01-28 00:16:34 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -91,8 +91,8 @@ hash = {
                                   label: "Genus",
                                       sub_category:{
                                           label: "Species",
-                                              sub_category: {
-                                                            nil
+                                              sub_category: { nil
+                                                            
                                               }
                                       }
                               }

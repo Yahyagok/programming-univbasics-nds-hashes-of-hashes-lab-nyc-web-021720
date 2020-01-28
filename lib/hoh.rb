dafa8a02be6c8f2e0316a8f45d44a5978ab8338c
Yahya  Gok <ygok17@hotmail.com>
2020-01-28 00:22:04 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -65,31 +65,31 @@ def naming_system
 
 
 
- {
-  label: "Kingdom",
-         sub_category: {
-            label: "Phylum",
-              sub_category: {
-                label: "Class",
-                  sub_category: {
-                    label: "Order",
-                        sub_category:{
-                          label: "Family",
-                              sub_category: {
-                                  label: "Genus",
-                                      sub_category:{
-                                          label: "Species",
-                                              sub_category: nil }
+# {
+#   label: "Kingdom",
+#         sub_category: {
+#             label: "Phylum",
+#               sub_category: {
+#                 label: "Class",
+#                   sub_category: {
+#                     label: "Order",
+#                         sub_category:{
+#                           label: "Family",
+#                               sub_category: {
+#                                   label: "Genus",
+#                                       sub_category:{
+#                                           label: "Species",
+#                                               sub_category: nil }
                                                             
                                               
-                                      }
-                              }
-                        }
-                  }
-              }
-        }
-      }
-}
+#                                       }
+#                               }
+#                         }
+#                   }
+#               }
+#         }
+#       }
+# }
 
 
 
